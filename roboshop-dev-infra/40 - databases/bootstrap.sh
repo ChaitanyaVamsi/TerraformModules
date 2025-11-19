b30d63a8ec57d0d691c20deff98390e28f3f67e1
chaitanya <vamsi.blaze1@gmail.com>
2025-11-19 23:50:21 +0530

    fix: updated

--- roboshop-dev-infra/40 - databases/bootstrap.sh
+++ roboshop-dev-infra/40 - databases/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 component=$1
-environment=$2
+env=$2
 dnf install ansible -y
 # ansible-pull -U https://github.com/ChaitanyaVamsi/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
@@ -22,4 +22,4 @@ else
   cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component -e environment=$environment main.yaml
+ansible-playbook -e component=$component -e env=$env main.yaml
